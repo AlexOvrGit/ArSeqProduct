e8e8e26c4a25db69ea4d70a68f0e05fdb84890ca
Alexandre Ovrutskiy <AlexOvr@gmx.us>
2025-08-26 14:51:19 -0400

    Add Worker2

--- TaskWorker.cs
+++ TaskWorker.cs
@@ -6,8 +6,7 @@ namespace ArSeqProduct
     {
         public static BigInteger Worker(int t, BigInteger m, Lock logLock)
         {
-            BigInteger result = m, s = Params.S, s2 = s * s, ds2 = 2 * s2, h = Params.H;
-
+            BigInteger result = m, s = Params.S, s2 = s * s, ds2 = 2 * s2, h = Params.H, n = Params.N;
             m *= m;
             for (; h > 0; h--)
             {
@@ -18,17 +17,43 @@ namespace ArSeqProduct
             //         return result;
             while (Params.R > 0)
             {
-                m = BigInteger.One;
                 lock (logLock)
                 {
                     if (Params.R <= 0) break;
-                    Params.N += Params.D;
+                    n += Params.D;
                     Params.R--;
                     m = Params.N;
                 }
                 result *= m;
             }
             return result;
+        }
+        public static (BigInteger even, BigInteger odd) Worker2(int t, BigInteger m, Lock logLock)
+        {
+            BigInteger m2 = 2 * m + 1, s = Params.S, s2 = s * s, ds2 = 2 * s2, h = Params.H, n = Params.N;
+            (BigInteger even, BigInteger odd) Big = (m, m + 1);
+            m *= m;
+            for (; h > 0; h--)
+            {
+                m -= s2;
+                Big.even *= m;
+                Big.odd *= (m + m2);
+                s2 += ds2;
+            }
+            //         return result;
+            while (Params.R > 0)
+            {
+                lock (logLock)
+                {
+                    if (Params.R <= 0) break;
+                    n += Params.D;
+                    Params.R--;
+                    m = Params.N;
+                }
+                Big.even *= m;
+                Big.odd *= (m + 1);
+            }
+            return Big;
         }
     }
 }
